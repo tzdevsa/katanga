2d32559aab9b735e1a6ceac34ad3e47b4b273cbb
Wongani <wonganibotha@outlook.com>
2025-01-31 14:52:52 +0200

    add reviews components

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,62 +1,34 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { getReviews } from "@/actions/getReviews";
 import { ReviewCard } from "@/components/ReviewCard";
-import { Typography, Grid, Container } from "@mui/material";
+import { Typography, Grid, Container, CardContent } from "@mui/material";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
-export default async function Testimonials() {
-  // Get the first three reviews with a rating greater than 4
-  const reviews = (await getReviews())?.filter((review: { rating: number; }) => review.rating > 4).splice(0, 3);
+interface Review {
+  profile_photo_url: string;
+  author_name: string;
+  rating: number;
+  text: string;
+  time: string;
+}
 
+export default async function Testimonials({ reviews }: { reviews: Review[] }) {
   return (
-    <section id="testimonials" style={{
-      backgroundColor: 'whitesmoke',
-    }}>
+    <section id="reviews">
       <Container>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <Typography
-              sx={{
-                display: { xs: 'none', md: 'block' }, // Larger screens only
-              }}
-              variant="h2"
-              fontWeight="bold"
-              align="center"
-              gutterBottom>
-              TESTIMONIALS
-            </Typography>
-            <Typography
-              sx={{
-                display: { xs: 'block', md: 'none' }, // Mobile only
-              }}
-              variant="h4"
-              fontWeight="bold"
-              align="left"
-              gutterBottom>
-              TESTIMONIALS
-            </Typography>
-            <Typography
-              sx={{
-                display: { xs: 'none', md: 'block' }, // Larger screens only
-              }}
-              variant="h4"
-              fontWeight="500"
-              align="center"
-              gutterBottom>
-              {'We value those we serve and their experiences. See what they have to say about us!'}
-            </Typography>
-            <Typography
-              sx={{
-                display: { xs: 'block', md: 'none' }, // Mobile only
-              }}
-              variant="h5"
-              fontWeight="500"
-              align="left"
-              gutterBottom>
-              {'We value those we serve and their experiences. See what they have to say about us!'}
-            </Typography>
+            <CardContent>
+              <Typography
+                variant="button"
+                component="h6"
+                align="left"
+                textTransform="capitalize">
+                Testimonials<ArrowRightIcon fontSize="small" color="primary" />
+              </Typography>
+            </CardContent>
           </Grid>
           {reviews.map((review: any) => (
-            <Grid item xs={12} sm={6} md={4} key={review.time}>
+            <Grid item xs={12} sm={6} md={3} key={review.time}>
               <ReviewCard
                 avatar={review.profile_photo_url}
                 name={review.author_name}
